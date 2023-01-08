772b4a9c1a8c4d6ab6640e3625f86447d8caa2d7
San Kang <devrivmt@gmail.com>
2023-01-08 22:07:17 +0900

    Rename: models.dart to model.dart

---
+++ lib/model/model.dart
@@ -0,0 +1,57 @@
+library my_api;
+
+import 'package:my_api/exceptions.dart';
+
+const String keyId = "id";
+const String keyPid = "pid";
+const String keyLastUsed = "last_used";
+const String keyOwner = "owner_id";
+const String keyEditors = "editors_id";
+const String keyDescriptions = "descriptions";
+
+class Model {
+
+  /// Raw data of this object
+  Map<String, dynamic> map = {};
+
+  /// Create object using [map]
+  Model(this.map);
+
+  /// Get value of [map] using [key]
+  ///
+  /// **DO NOT** call this directly. Use several properties variables to access.
+  /// When [key] is not included in [map], throws [InvalidModeException].
+  getValue(String key) {
+    if (map.containsKey(key)) {
+      return map[key];
+    }
+    throw InvalidModelException(key);
+  }
+
+  /// ID (Read-only)
+  int get id => getValue(keyId);
+
+  /// PID (Read-only)
+  int get pid => getValue(keyPid);
+
+  /// [DateTime] of lastly used
+  DateTime get lastUsed => DateTime.fromMillisecondsSinceEpoch(getValue(keyLastUsed));
+
+  set lastUsed(DateTime dateTime) => map[keyLastUsed] = dateTime.millisecondsSinceEpoch;
+
+  /// UID of owner
+  String get owner => getValue(keyOwner);
+
+  set owner(String id) => throw UnimplementedError();
+
+  /// List of editor UID
+  List<String> get editors => getValue(keyEditors);
+
+  set editors(List<String> list) => map[keyEditors] = list;
+
+  /// Descriptions of this object
+  String get descriptions => getValue(keyDescriptions);
+
+  set descriptions(String desc) => map[keyDescriptions] = desc;
+
+}
