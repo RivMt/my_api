30e93890aa12c6e7eb93ca54194e01dc02b5a883
San Kang <san.kang@outlook.com>
2023-01-09 18:56:59 +0900

    Add: Request related exceptions
    UriChanged, Invalid, Non-auth, Permission denied, other error

--- lib/exceptions.dart
+++ lib/exceptions.dart
@@ -1,8 +1,42 @@
-
-
+/// Exception for Model is not valid
 class InvalidModelException implements Exception {
   final String key;
 
   InvalidModelException(this.key);
 
+}
+
+/// Exception for requested URI has been changed (Code 301)
+class RequestedUriChangedException implements Exception {
+  final String url;
+
+  RequestedUriChangedException(this.url);
+}
+
+/// Exception for invalid request (Code 400)
+class RequestInvalidException implements Exception {
+  final String url;
+
+  RequestInvalidException(this.url);
+}
+
+/// Exception for Non-auth user request (Code 401)
+class RequestNonAuthenticatedException implements Exception {
+  final String url;
+
+  RequestNonAuthenticatedException(this.url);
+}
+
+/// Exception for request has been denied for permission reason (Code 405)
+class RequestPermissionDeniedException implements Exception {
+  final String url;
+
+  RequestPermissionDeniedException(this.url);
+}
+
+/// Exception for any other error (Code 500)
+class RequestErrorException implements Exception {
+  final String url;
+
+  RequestErrorException(this.url);
 }
