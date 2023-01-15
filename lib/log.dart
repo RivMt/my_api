8f92060dd11ad9839177dfebeea780a68de08d05
San Kang <san.kang@outlook.com>
2023-01-15 17:24:44 +0900

    Add: Log system

---
+++ lib/log.dart
@@ -0,0 +1,56 @@
+library my_api;
+
+import 'dart:io';
+
+enum LogLevel {
+  error,
+  warning,
+  debug,
+  info,
+  verbose,
+}
+
+class Log {
+
+  static LogLevel level = LogLevel.verbose;
+
+  static void _print(LogLevel level, String color, String? tag, Object? msg) {
+    // Do not log its level is lower than level
+    if (level.index > Log.level.index) {
+      return;
+    }
+    // Set color
+    stdout.write(color);
+    // Print tag
+    if (tag == null) {
+      stdout.write("[${level.name}] ");
+    } else {
+      stdout.write("[${level.name.substring(0,1).toUpperCase()}/$tag] ");
+    }
+    // Print msg
+    stdout.write("${msg ?? ""}");
+    // Reset color and print newline
+    stdout.write("\x1B[0m\n");
+  }
+
+  static void e([String? tag, Object? msg]) {
+    _print(LogLevel.error, "\x1B[31m", tag, msg);
+  }
+
+  static void w([String? tag, Object? msg]) {
+    _print(LogLevel.warning, "\x1B[33m", tag, msg);
+  }
+
+  static void d([String? tag, Object? msg]) {
+    _print(LogLevel.debug, "\x1B[32m", tag, msg);
+  }
+
+  static void i([String? tag, Object? msg]) {
+    _print(LogLevel.info, "\x1B[36m", tag, msg);
+  }
+
+  static void v([String? tag, Object? msg]) {
+    _print(LogLevel.verbose, "", tag, msg);
+  }
+
+}
