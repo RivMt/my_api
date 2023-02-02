ffabe139f67629765574a0d5e6e9c795b9eddb56
San Kang <san.kang@outlook.com>
2023-02-02 11:41:46 +0900

    Change: `GroupCard`
    Add default width and height
    Add onMorePressed which triggers on right arrow button
    Add padding and edit layout make beautiful more

--- lib/src/component/group_card.dart
+++ lib/src/component/group_card.dart
@@ -6,13 +6,26 @@ class GroupCard extends StatefulWidget {
     required this.title,
     required this.count,
     required this.build,
+    this.onMorePressed,
   });
 
+  /// Default width
+  static const int width = 480;
+
+  /// Default height
+  static const int height = 240;
+
+  /// Title
   final String title;
 
+  /// Number of children
   final int count;
 
+  /// Build list
   final Widget? Function(BuildContext, int) build;
+
+  /// Triggers on right arrow button pressed
+  final Function()? onMorePressed;
 
   @override
   _GroupCardState createState() => _GroupCardState();
@@ -22,28 +35,40 @@ class _GroupCardState extends State<GroupCard> {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          // Title
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                widget.title,
-                style: Theme.of(context).textTheme.titleMedium,
-              )
-            ],
-          ),
-          // Widgets
-          ListView.builder(
-            shrinkWrap: true,
-            itemCount: widget.count,
-            itemBuilder: widget.build,
-          ),
-        ],
+      child: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            // Title
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Wrap(
+                  children: [
+                    const SizedBox(width: 8,),
+                    Text(
+                      widget.title,
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
+                  ],
+                ),
+                IconButton(
+                  icon: const Icon(Icons.chevron_right_outlined),
+                  onPressed: widget.onMorePressed,
+                )
+              ],
+            ),
+            // Widgets
+            ListView.builder(
+              shrinkWrap: true,
+              itemCount: widget.count,
+              itemBuilder: widget.build,
+            ),
+          ],
+        ),
       ),
     );
   }
