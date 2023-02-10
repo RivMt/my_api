61a852b88255e18b2bc197a87f78b4192094eb79
San Kang <san.kang@outlook.com>
2023-02-10 11:47:33 +0900

    Refactor: `UserGender`

--- lib/src/model/user.dart
+++ lib/src/model/user.dart
@@ -37,7 +37,7 @@ class User extends Model {
   set birthday(DateTime date) => map[keyBirthday] = date.millisecondsSinceEpoch;
 
   /// Gender
-  UserGender get gender => UserGender.find(getValue(keyGender, ""));
+  UserGender get gender => UserGender.fromName(getValue(keyGender, ""));
 
   set gender(UserGender gender) => map[keyGender] = gender.name;
 
@@ -54,28 +54,68 @@ class User extends Model {
 
 class UserGender {
 
+  /// Code for Gender OTHER
+  static const int codeOther = -1;
+
+  /// Code for Gender MALE
+  static const int codeMale = 0;
+
+  /// Code for Gender FEMALE
+  static const int codeFemale = 1;
+
   /// Constant for Gender MALE
-  static final UserGender male = UserGender("male");
+  static final UserGender male = UserGender(codeMale, "male");
 
   /// Constant for Gender FEMALE
-  static final UserGender female = UserGender("female");
+  static final UserGender female = UserGender(codeFemale, "female");
 
   /// Users can define gender themself
-  UserGender(this.name);
+  UserGender(this.code, this.name);
 
   /// Name of gender
   final String name;
 
+  /// Code of gender
+  final int code;
+
   /// Find gender using [name]
-  static UserGender find(String name) {
+  static UserGender fromName(String name) {
     switch(name.toLowerCase()) {
       case "male":
         return male;
       case "female":
         return female;
       default:
-        return UserGender(name);
+        return UserGender(codeOther, name);
     }
+  }
+
+  /// Find gender using [code]
+  ///
+  /// If [code] is [codeOther], returns [name] gender.
+  static UserGender fromCode(int code, [String name = ""]) {
+    switch(code) {
+      case codeMale:
+        return male;
+      case codeFemale:
+        return female;
+      default:
+        return UserGender(codeOther, name);
+    }
+  }
+
+  @override
+  int get hashCode => name.hashCode;
+
+  @override
+  bool operator ==(Object other) {
+    if (other is UserGender) {
+      if (code == other.code && code == codeOther) {
+        return name == other.name;
+      }
+      return code == other.code;
+    }
+    return super == other;
   }
 
   /// Display gender name
