b1fdc30bd3c22cf510f2f2a47e28b1e2604eede5
San Kang <san.kang@outlook.com>
2023-02-14 19:32:04 +0900

    Feat: `InterfaceConstructor`
    Add `isSidePanelVisible` to check side panel is visible
    Add `panelWidth` to calculate each panel's width

--- lib/core/widget/interface_constructor.dart
+++ lib/core/widget/interface_constructor.dart
@@ -2,8 +2,20 @@ import 'package:flutter/material.dart';
 
 abstract class InterfaceConstructor {
 
+  /// Threshold ratio of width per height to check wide screen
   static const _standardRatio = 0.8;
 
+  /// Value of additional panel is visible or not
+  static bool isSidePanelVisible(BuildContext context) {
+    return panelNumber(context) > 1;
+  }
+
+  /// Width of each panel
+  static double panelWidth(BuildContext context) {
+    return MediaQuery.of(context).size.width / panelNumber(context);
+  }
+
+  /// Calculate number of panels from [context]
   static int panelNumber(BuildContext context) {
     final double width, height;
     width = MediaQuery.of(context).size.width;
