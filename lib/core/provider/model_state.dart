a63f3d1a2b54b602dff9b5b620971730a788163e
San Kang <san.kang@outlook.com>
2023-02-23 17:04:03 +0900

    Fix: Change return type of `request` to `Future`

--- lib/core/provider/model_state.dart
+++ lib/core/provider/model_state.dart
@@ -14,7 +14,7 @@ class ModelsState<T> extends StateNotifier<List<T>> {
   void clear() => state = [];
 
   /// Request [T] items fit to [condition] and filter by [options]
-  void request(List<Map<String, dynamic>> condition, [Map<String, dynamic>? options]) async {
+  Future<void> request(List<Map<String, dynamic>> condition, [Map<String, dynamic>? options]) async {
     final client = ApiClient();
     final ApiResponse<List<T>> response = await client.read<T>(
       condition,
@@ -39,7 +39,7 @@ class ModelState<T> extends StateNotifier<T?> {
   void clear() => state = null;
 
   /// Request [T] items fit to [condition] and filter by [options]
-  void request(Map<String, dynamic> condition, [Map<String, dynamic>? options]) async {
+  Future<void> request(Map<String, dynamic> condition, [Map<String, dynamic>? options]) async {
     final client = ApiClient();
     final ApiResponse<List<T>> response = await client.read<T>(
       [condition],
