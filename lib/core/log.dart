29b5ce97908e41336f20e863851eaf8287b34c31
San Kang <devrivmt@gmail.com>
2023-04-01 13:41:10 +0900

    Feat: Upgrade log system

--- lib/core/log.dart
+++ lib/core/log.dart
@@ -1,64 +1,63 @@
 library my_api;
 
-import 'dart:io';
-
-import 'package:flutter/foundation.dart';
+import 'dart:developer';
 
 enum LogLevel {
-  error,
-  warning,
-  debug,
-  info,
-  verbose,
+  error(4, "E", "Error"),
+  warning(3, "W", "Warning"),
+  debug(2, "D", "Debug"),
+  info(1, "I", "Info"),
+  verbose(0, "V", "Verbose");
+
+  final int level;
+
+  final String letter;
+
+  final String code;
+
+  const LogLevel(this.level, this.letter, this.code);
+
+  @override
+  String toString() => code;
 }
 
 class Log {
 
   static LogLevel level = LogLevel.verbose;
 
-  static void _print(LogLevel level, String color, String? tag, Object? msg) {
+  static void _print(LogLevel level, String color, String? tag, Object? msg, [Object? e, StackTrace? s]) {
     // Do not log its level is lower than level
-    if (level.index > Log.level.index) {
+    if (level.level < Log.level.level) {
       return;
     }
-    if (kIsWeb) {
-      if (kDebugMode) {
-        print("[${level.name.substring(0, 1).toUpperCase()}/$tag] $msg");
-      }
-    } else {
-      // Set color
-      stdout.write(color);
-      // Print tag
-      if (tag == null) {
-        stdout.write("[${level.name}] ");
-      } else {
-        stdout.write("[${level.name.substring(0, 1).toUpperCase()}/$tag] ");
-      }
-      // Print msg
-      stdout.write("${msg ?? ""}");
-      // Reset color and print newline
-      stdout.write("\x1B[0m\n");
-    }
+    log(
+      "$color$msg\x1B[0m",
+      name: "${level.letter}/${tag ?? ""}",
+      time: DateTime.now(),
+      level: level.level,
+      error: e,
+      stackTrace: s,
+    );
   }
 
-  static void e([String? tag, Object? msg]) {
-    _print(LogLevel.error, "\x1B[31m", tag, msg);
+  static void e([String? tag, Object? msg, Object? e, StackTrace? s]) {
+    _print(LogLevel.error, "\x1B[31m", tag, msg, e, s);
   }
 
-  static void w([String? tag, Object? msg]) {
-    _print(LogLevel.warning, "\x1B[33m", tag, msg);
+  static void w([String? tag, Object? msg, Object? e, StackTrace? s]) {
+    _print(LogLevel.warning, "\x1B[33m", tag, msg, e, s);
   }
 
-  static void d([String? tag, Object? msg]) {
-    _print(LogLevel.debug, "\x1B[32m", tag, msg);
+  static void d([String? tag, Object? msg, Object? e, StackTrace? s]) {
+    _print(LogLevel.debug, "\x1B[32m", tag, msg, e, s);
   }
 
-  static void i([String? tag, Object? msg]) {
-    _print(LogLevel.info, "\x1B[36m", tag, msg);
+  static void i([String? tag, Object? msg, Object? e, StackTrace? s]) {
+    _print(LogLevel.info, "\x1B[36m", tag, msg, e, s);
   }
 
-  static void v([String? tag, Object? msg]) {
-    _print(LogLevel.verbose, "", tag, msg);
+  static void v([String? tag, Object? msg, Object? e, StackTrace? s]) {
+    _print(LogLevel.verbose, "", tag, msg, e, s);
   }
 
 }
