f87be922f24ba88186d4fd391801987b383023af
San Kang <devrivmt@gmail.com>
2024-02-19 22:58:45 +0900

    Refactor: `User` and `LoginPage`
    Clean up code and remove inspector warnings

--- lib/core/model/user.dart
+++ lib/core/model/user.dart
@@ -5,7 +5,7 @@ class User extends Model {
 
   static const int minPasswordLength = 8;
 
-  User(super.map);
+  User([super.map]);
 
   /// Check password validation
   static bool checkPassword(String password) {
