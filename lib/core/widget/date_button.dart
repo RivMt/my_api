e8d6c18bb4da164e1a32f322c0ffc59555d4f5a1
San Kang <devrivmt@gmail.com>
2024-09-30 12:44:11 +0900

    Refactor: `DateButton`

--- lib/core/widget/date_button.dart
+++ lib/core/widget/date_button.dart
@@ -1,22 +1,42 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:my_api/core/model/model.dart';
 
 class DateButton extends StatelessWidget {
   const DateButton({
     super.key,
     required this.date,
-    this.onTap,
+    this.onChanged,
   });
 
   final DateTime date;
 
-  final Function()? onTap;
+  final Function(DateTime)? onChanged;
+
+  /// Show date picker
+  Future<DateTime> showDatePickDialog(BuildContext context, DateTime base) async {
+    final DateTime? result = await showDatePicker(
+      context: context,
+      initialDate: base,
+      firstDate: DateTime.fromMillisecondsSinceEpoch(0),
+      lastDate: Model.maxDate,
+    );
+    return result ?? base;
+  }
+
+  /// Triggers on paid date button pressed
+  void onDateButtonPressed(BuildContext context) async {
+    final result = await showDatePickDialog(context, date);
+    if (onChanged != null) {
+      onChanged!(result);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
       borderRadius: BorderRadius.circular(8),
-      onTap: onTap,
+      onTap: () => onDateButtonPressed(context),
       child: Padding(
         padding: const EdgeInsets.all(8),
         child: Wrap(
